c7dcee124a656aa93a3375ad510c804d98fb0d8b
Keijiro Takahashi <keijiro@gmail.com>
2018-07-16 14:26:48 +0900

    Update with Unity 2018.2

--- Assets/ParticleSystemControl/Editor/ParticleSystemControlTrackEditor.cs
+++ Assets/ParticleSystemControl/Editor/ParticleSystemControlTrackEditor.cs
@@ -4,35 +4,8 @@
 using UnityEditor;
 using UnityEditor.Timeline;
 
-// We use a nested editor to enable context reference (it's needed to enable
-// exposed references).
-
 [CustomEditor(typeof(ParticleSystemControlTrack)), CanEditMultipleObjects]
 class ParticleSystemControlTrackEditor : Editor
-{
-    Editor _editor;
-
-    void OnEnable()
-    {
-        // Use the current active director as an editing context.
-        _editor = Editor.CreateEditorWithContext(
-            targets, TimelineEditor.playableDirector,
-            typeof(ParticleSystemControlTrackEditor2)
-        );
-    }
-
-    void OnDestroy()
-    {
-        DestroyImmediate(_editor);
-    }
-
-    public override void OnInspectorGUI()
-    {
-        _editor.OnInspectorGUI();
-    }
-}
-
-class ParticleSystemControlTrackEditor2 : Editor
 {
     SerializedProperty _snapTarget;
     SerializedProperty _randomSeed;
